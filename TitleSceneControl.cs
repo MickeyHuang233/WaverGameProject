6fe0d761d5c0f8bac82b6d8647716e01c3561e90
MickeyHuang233 <41979831+MickeyHuang233@users.noreply.github.com>
2019-05-27 14:58:21 +0800

    新增說明、讀取游戲及關閉遊戲的空方法

--- TitleSceneControl.cs
+++ TitleSceneControl.cs
@@ -3,26 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-//控制遊戲主頁面
+/*
+    遊戲初始畫面的Button方法
+    掛載物件：
+        TitleSceneControl --> 
+    實現功能：
+        1. 遊戲開始後加載初始化場景 // TODO 畫面需要淡出
+        2. // TODO 遊戲讀取的操作
+        3. // TODO 遊戲關閉的操作
+*/
 public class TitleSceneControl : MonoBehaviour
 {
+    //初始化場景名
     public string startScene;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
+    //遊戲開始
     public void doGameStart()
     {
         //Application.LoadLevel("Scene_1000");//舊方法，可透過場景名或場景編號加載場景
         SceneManager.LoadScene(startScene);//新的加找場景方法
+    }
+    
+    //讀取遊戲
+    public void doGameLoad()
+    {
+        
+    }
+    
+    //關閉遊戲
+    public void doGameExit()
+    {
+    
     }
 }
