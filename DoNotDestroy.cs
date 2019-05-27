f64e3beebdd8742f9ed7b1b01833f38b2a2f2b23
MickeyHuang233 <41979831+MickeyHuang233@users.noreply.github.com>
2019-05-27 14:42:24 +0800

    新增說明

--- DoNotDestroy.cs
+++ DoNotDestroy.cs
@@ -2,7 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//掛上此元件的話，此物件在場景轉換不會被unity釋放掉
+/*
+    掛上此元件的話，此物件在場景轉換不會被unity釋放掉
+    掛載物件：
+        DoNotDestroy --> 場景轉換不需要被釋放的物件
+    實現功能：
+        在場景轉換不會被unity釋放掉
+*/
 public class DoNotDestroy : MonoBehaviour
 {
     void Start()
