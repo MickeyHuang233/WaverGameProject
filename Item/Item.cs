56ba92b5b618daf3ee1819b3fe1b144fe987286d
MickeyHuang233 <mickeyprojectmail233@gmail.com>
2019-06-01 13:35:58 +0800

    新增初始化獲得道具及取得和失去道具的方法

---
+++ Item/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    //物品編號
+    private int itemId;
+    public int ItemId { get; set; }
+
+    //物品名稱
+    private string itemName;
+    public string ItemName { get; set; }
+
+    //物品說明
+    private string description;
+    public string Description { get; set; }
+
+    //物品使用域判斷的物件名
+    private string useItemameObject;
+    public string UseItemameObject { get; set; }
+
+}
