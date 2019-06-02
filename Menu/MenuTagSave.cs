36bee83ea3948517a18d96ce426d2b1f8734af9b
MickeyHuang <mickeyprojectmail233@gmail.com>
2019-06-02 16:22:09 +0800

    新增存檔菜單的指標移動

--- Menu/MenuTagSave.cs
+++ Menu/MenuTagSave.cs
@@ -10,14 +10,42 @@ public class MenuTagSave : MonoBehaviour
     //存檔菜單文字的動畫信息
     private Animator savePageAanimator;
 
+    //Menu物件
+    GameObject MenuObject;
+
+    //指標物件
+    GameObject itemIndex;
+
+    //當前位置編號
+    private int tagIndex = 1;
+
+    //最大位置編號
+    private int tagIndexMax;
+
     void Start()
     {
+        //取得savePage物件及動畫信息
         savePage = GameObject.Find("SavePage");
         savePageAanimator = savePage.GetComponent<Animator>();
+        //取得Menu物件
+        MenuObject = this.transform.parent.parent.gameObject;
+        //取得指標物件
+        itemIndex = this.transform.GetChild(2).gameObject.transform.GetChild(0).gameObject;
+        //取得最大位置編號，要去掉指標物件
+        tagIndexMax = this.transform.GetChild(2).gameObject.transform.childCount - 1;
+        //初始化指標物件的位置
+        Vector3 choicePosition = this.transform.GetChild(2).gameObject.transform.GetChild(tagIndex).gameObject.transform.position;
+        itemIndex.transform.position = new Vector3(choicePosition.x - 0.25F, choicePosition.y, choicePosition.z);
     }
 
     void Update()
     {
+        float v = Input.GetAxisRaw("Vertical");//檢測垂直移動
+        if (PlayerItemMenu.openDetailMenu == 2)
+        {
+            if (Input.GetButtonDown("Vertical")) doMove(v);
+            if (Input.GetKeyDown(KeyCode.Z) && PlayerItemMenu.overRestTime) doSubmit();
+        }
 
     }
 
@@ -34,4 +62,25 @@ public class MenuTagSave : MonoBehaviour
         savePageAanimator.SetBool("openDetilMenu", false);
         savePageAanimator.SetBool("closeDetilMenu", true);
     }
+
+    #region 指標移動
+    private void doMove(float v)
+    {
+        if (v > 0 && tagIndex > 1) tagIndex--;//第一項，向上移動
+        else if (v > 0 && tagIndex == 1) tagIndex = tagIndexMax;//向上移動
+        else if (v < 0 && tagIndex < tagIndexMax) tagIndex++;//向下移動
+        else if (v < 0 && tagIndex == tagIndexMax) tagIndex = 1;//最後一項，向下移動
+        //更新指標位置
+        Vector3 choicePosition = this.transform.GetChild(2).gameObject.transform.GetChild(tagIndex).gameObject.transform.position;
+        itemIndex.transform.position = new Vector3(choicePosition.x - 0.25F, choicePosition.y, choicePosition.z);
+    }
+    #endregion
+
+    #region 按下確認鍵操作
+    private void doSubmit()
+    {
+        Debug.Log("Save_doSubmit()");
+        PlayerItemMenu.returnRestTimer();
+    }
+    #endregion
 }
