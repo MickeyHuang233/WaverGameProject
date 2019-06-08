1aeeb0d03713766e48c76a37f5dc5a5c50b0a8a5
MickeyHuang233 <mickeyprojectmail233@gmail.com>
2019-06-08 21:39:02 +0800

    新增主頁面選項移動和確認操作

--- TitleSceneControl.cs
+++ TitleSceneControl.cs
@@ -6,11 +6,11 @@ using UnityEngine.SceneManagement;
 /*
     遊戲初始畫面的Button方法
     掛載物件：
-        TitleSceneControl --> 
+        TitleSceneControl --> TitleScenesManager
     實現功能：
-        1. 遊戲開始後加載初始化場景 // TODO 畫面需要淡出
+        1. 遊戲開始後加載初始化場景
         2. // TODO 遊戲讀取的操作
-        3. // TODO 遊戲關閉的操作
+        3. 遊戲關閉的操作
 */
 public class TitleSceneControl : MonoBehaviour
 {
@@ -23,17 +23,41 @@ public class TitleSceneControl : MonoBehaviour
     //相機物件
     private GameObject cameraObject;
 
+    //主頁面按鍵物件
+    private List<GameObject> menuButtonObject = new List<GameObject>();
+
+    //指標物件
+    private GameObject itemIndex;
+
+    //當前位置編號
+    private int tagIndex = 1;
+
+    //最大位置編號
+    private int tagIndexMax;
+
     #region Start()
     void Start()
     {
         cameraObject = GameObject.Find("Main Camera");
+        //取得最大位置編號，要去掉指標物件
+        tagIndexMax = GameObject.Find("Canvas_TitleButton").transform.childCount - 1;
+        //獲取主頁面按鍵物件
+        for (int i=1; i<=tagIndexMax; i++) menuButtonObject.Add(GameObject.Find("Canvas_TitleButton").transform.GetChild(i).gameObject);
+        //獲取指標物件
+        itemIndex = GameObject.Find("Canvas_TitleButton").transform.GetChild(0).gameObject;
+        //初始化指標物件的位置
+        Vector3 choicePosition = menuButtonObject[tagIndex-1].transform.position;
+        itemIndex.transform.position = new Vector3(choicePosition.x - 0.25F, choicePosition.y, choicePosition.z);
     }
     #endregion
 
     #region Update()
     void Update()
     {
-        if (Input.GetButtonDown("Submit")) doGameStart();//暫時使用
+        float v = Input.GetAxisRaw("Vertical");//檢測垂直移動
+        if (Input.GetButtonDown("Vertical")) doMove(v);
+        if (Input.GetButtonDown("Submit")) doSubmit();
+        //根據selectButton的關鍵字決定要做什麼操作
         if (selectButton.Equals("Start"))
         {
             if(CameraFix.sceneGradientIsStatus("SceneDark")) SceneManager.LoadScene(startScene);//新的加找場景方法
@@ -51,10 +75,25 @@ public class TitleSceneControl : MonoBehaviour
     }
     #endregion
 
-    #region 遊戲開始
-    public void doGameStart()
+    #region 指標移動
+    private void doMove(float v)
     {
-        selectButton = "Start";
+        if (v > 0 && tagIndex > 1) tagIndex--;//第一項，向上移動
+        else if (v > 0 && tagIndex == 1) tagIndex = tagIndexMax;//向上移動
+        else if (v < 0 && tagIndex < tagIndexMax) tagIndex++;//向下移動
+        else if (v < 0 && tagIndex == tagIndexMax) tagIndex = 1;//最後一項，向下移動
+        //更新指標位置
+        Vector3 choicePosition = menuButtonObject[tagIndex - 1].transform.position;
+        itemIndex.transform.position = new Vector3(choicePosition.x - 0.25F, choicePosition.y, choicePosition.z);
+    }
+    #endregion
+
+    #region 按下確認鍵操作
+    private void doSubmit()
+    {
+        if (tagIndex == 1) selectButton = "Start";//Start
+        else if (tagIndex ==2) doGameLoad();//Load
+        else if (tagIndex ==3) selectButton = "Exit";//Exit
     }
     #endregion
 
@@ -63,14 +102,6 @@ public class TitleSceneControl : MonoBehaviour
     {
         Debug.Log("doGameLoad");
         selectButton = "Load";
-    }
-    #endregion
-
-    #region 關閉遊戲
-    public void doGameExit()
-    {
-        Debug.Log("doGameExit");
-        selectButton = "Exit";
     }
     #endregion
 }
