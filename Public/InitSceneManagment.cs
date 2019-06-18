8f85a07b3531a38a2173cecf5425144fae8f4e4a
MickeyHuang233 <mickeyprojectmail233@gmail.com>
2019-06-18 22:34:59 +0800

    移除前景黑的操作

--- Public/InitSceneManagment.cs
+++ Public/InitSceneManagment.cs
@@ -13,15 +13,6 @@ public class InitSceneManagment : MonoBehaviour
     //玩家物件
     private GameObject playerObject;
 
-    //用來管理DarkLayout的物件
-    private Transform darkLayoutHolder;
-
-    [Header("地圖類型")]
-    public string mapType;
-
-    [Header("1F顯示物件")]
-    public GameObject[] darkLayout_1F;
-
     //傳送位置物件名稱，主要從Portal賦值
     public static string targetPositionName { get => positionName; set => positionName = value; }
 
@@ -64,9 +55,6 @@ public class InitSceneManagment : MonoBehaviour
 
         getTargetPosition(); //取得指定傳送點物件
 
-        createDarkLayout();//建立前景黑的物件
-        darkLayoutToList();//將DarkLayout的全部子物件放入List中
-
         //加載GameManager
         Talkable.flowchartManager = GameObject.Find("SpeakManager").GetComponent<Flowchart>();
 
@@ -89,7 +77,6 @@ public class InitSceneManagment : MonoBehaviour
     {
         if (!showedSceneName) showSceneName();//顯示場景名稱
         if (GameTimer.nactivesecond >= 0) showNagativeTime();
-        darkLayoutMove();//DarkLayout的移動
         loadTargetScene();//場景移動
     }
     #endregion
@@ -159,61 +146,6 @@ public class InitSceneManagment : MonoBehaviour
             (GameTimer.nactivesecond >= 10) ? "0" + GameTimer.nactivesecond.ToString() + "秒" : 
             "00" + GameTimer.nactivesecond.ToString() + "秒";
         CameraFix.showNagativeTime(timer);
-    }
-    #endregion
-
-    #region 建立前景黑的物件    createDarkLayout()
-    private void createDarkLayout()
-    {
-        darkLayoutHolder = new GameObject("DarkLayout").transform;//建立一個新的物件叫Map，並取得這個物件的transform屬性
-        if (mapType.Equals("1F"))//依地圖類型分産出的DarkLayout物件，默認是什麼都不産出
-        {
-            for (int i = 0; i < darkLayout_1F.Length; i++)
-            {
-                GameObject go = GameObject.Instantiate(darkLayout_1F[i], new Vector3(playerObject.transform.position.x, playerObject.transform.position.y, 0), Quaternion.identity) as GameObject;
-                go.gameObject.GetComponent<Renderer>().material.color = new Color(0, 0, 0, 0.5F);
-                go.transform.SetParent(darkLayoutHolder);//將新建出來的地圖放到darkLayoutHolder物件底下
-            }
-        }
-        else if (mapType.Equals("3F"))
-        {
-            //TODO 需加3F的DarkLayout産生
-        }
-        else if (mapType.Equals("B1"))
-        {
-            //TODO 需加B1的DarkLayout産生
-        }
-    }
-    #endregion
-
-    #region 將DarkLayout的全部子物件放入List中    darkLayoutToList()
-    private void darkLayoutToList()
-    {
-        darkLayoutObject = GameObject.Find("DarkLayout");
-        for (int i = 0; i < darkLayoutObject.transform.GetChildCount(); i++)
-        {
-            GameObject d = darkLayoutObject.transform.GetChild(i).gameObject;
-            darkLayoutObjects.Add(d);
-        }
-    }
-    #endregion
-
-    #region DarkLayout的移動   darkLayoutMove()
-    private void darkLayoutMove()
-    {
-        float v = (PlayerMovement.moveDirction.x > 0) ? 0.5F : (PlayerMovement.moveDirction.x == 0) ? 0F : -0.5F;
-        float h = (PlayerMovement.moveDirction.y > 0) ? 0.5F : (PlayerMovement.moveDirction.y == 0) ? 0F : -0.5F;
-        //取得玩家位置
-        float playerX = playerObject.transform.position.x + v;
-        float playerY = playerObject.transform.position.y + h;
-
-        for (int i = 0; i < darkLayoutObjects.Count; i++)
-        {
-            //TODO 使用蒙版實現場景渲染
-            GameObject d = darkLayoutObjects[i];
-            darkLayoutTargetPosition = new Vector3(playerX, playerY, 0);
-            d.transform.position = Vector3.Lerp(d.transform.position, darkLayoutTargetPosition, moveSpeed * Time.deltaTime / (i+1));
-        }
     }
     #endregion
 
