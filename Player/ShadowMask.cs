0d29138e4626fafafbe24585b2dcb682bb77dd5d
MickeyHuang233 <mickeyprojectmail233@gmail.com>
2019-06-24 23:26:30 +0800

    調整打開菜單時的光線大小及位置

--- Player/ShadowMask.cs
+++ Player/ShadowMask.cs
@@ -7,6 +7,9 @@ using UnityEngine;
 */
 public class ShadowMask : MonoBehaviour
 {
+    [Header("手電筒類別")]
+    public string ShadowMaskType;
+
     [Header("等待時間范圍")]
     [Range(0.5F, 10F)]
     public float restTime = 5F;
@@ -17,11 +20,13 @@ public class ShadowMask : MonoBehaviour
 
     [Header("調整X軸")]
     [Range(-1.0F, 1.0F)]
-    public float modifyX = 0F;
+    public float ModifyX = 0F;
 
     [Header("調整Y軸")]
     [Range(-1.0F, 1.0F)]
-    public float modifyY = 0F;
+    public float ModifyY = 0F;
+
+    public float playerModify = 0.5F;
 
     [Header("DarkLayout移動速度")]
     [Range(0F, 50F)]
@@ -42,13 +47,21 @@ public class ShadowMask : MonoBehaviour
     [Header("目標跟隨物體是否是菜單")]
     public bool isMenu = false;
 
-    //菜單大小
-    private float menuScaleX = 0.4F;
-    private float menuScaleY = 0.4F;
+    //手電筒大小，暫存
+    private float scaleX;
+    private float scaleY;
+
+    //手電絧偏移量，暫存
+    private float modifyX;
+    private float modifyY;
 
     #region Start()
     public void Start()
     {
+        scaleX = transform.localScale.x;
+        scaleY = transform.localScale.y;
+        modifyX = ModifyX;
+        modifyY = ModifyY;
         if (isMenu) transform.localScale = new Vector3(0, 0, 0);
     }
     #endregion
@@ -56,19 +69,63 @@ public class ShadowMask : MonoBehaviour
     #region Update()
     public void Update()
     {
-        RestTimer += Time.deltaTime;
-        if (isMenu && PlayerItemMenu.openDetailMenu > -1) darkLayoutShine();//超過等待時間，燈光閃一下
-        darkLayoutMove();//DarkLayout的移動
-        if (isMenu) menuTriggerDarkLayout();
+        if (!ShadowMaskType.Equals(InitSceneManagment.mapLayoutType))
+        {
+            transform.localScale = new Vector3(0, 0, 0);
+        }
+        else
+        {
+            transform.localScale = new Vector3(scaleX, scaleY, 0);
+            RestTimer += Time.deltaTime;
+            if (!isMenu || PlayerItemMenu.openDetailMenu > 0) darkLayoutShine();//超過等待時間，燈光閃一下
+            if (isMenu) menuTriggerDarkLayout();
+            if (isPlayer) playerTriggerDarkLayout();
+            darkLayoutMove();//DarkLayout的移動
+        }
     }
     #endregion
 
     #region 菜單顯示或隱藏DarkLayout   menuTriggerDarkLayout()
     private void menuTriggerDarkLayout()
     {
+        float timer = Time.deltaTime * 6;
         //當玩家打開一級菜單時另外顯示光源
-        if (PlayerItemMenu.openDetailMenu > -1) transform.localScale = new Vector3(menuScaleX, menuScaleY, 0);
-        else transform.localScale = new Vector3(0, 0, 0);
+        if (PlayerItemMenu.openDetailMenu < 0)//沒打開菜單
+        {
+            transform.localScale = new Vector3(0, 0, 0);
+            ModifyX = (ModifyX <= 0.005) ? 0 : Mathf.Lerp(ModifyX, 0, timer);
+            ModifyY = (ModifyY <= 0.005) ? 0 : Mathf.Lerp(ModifyY, 0, timer);
+        }
+        else if (PlayerItemMenu.openDetailMenu == 0)//打開一級菜單
+        {
+            transform.localScale = new Vector3(scaleX / 2, scaleY / 2, 0);
+            ModifyX = Mathf.Lerp(ModifyX, modifyX / 2, timer);
+            ModifyY = Mathf.Lerp(ModifyY, modifyY / 2, timer);
+        }
+        else//打開二級菜單
+        {
+            transform.localScale = new Vector3(scaleX, scaleY, 0);
+            ModifyX = (ModifyX >= modifyX * 0.99) ? modifyX : Mathf.Lerp(ModifyX, modifyX, timer);
+            ModifyY = (ModifyY >= modifyY * 0.99) ? modifyY : Mathf.Lerp(ModifyY, modifyY, timer);
+        }
+    }
+    #endregion
+
+    #region 菜單顯示或隱藏時玩家的DarkLayout   playerTriggerDarkLayout()
+    private void playerTriggerDarkLayout()
+    {
+        float timer = Time.deltaTime * 6;
+        //當玩家打開一級菜單時另外顯示光源
+        if (PlayerItemMenu.openDetailMenu < 0)//沒打開菜單
+        {
+            //transform.localScale = new Vector3(scaleX, scaleY, 0);
+            playerModify = 0.5F;
+        }
+        else//打開菜單
+        {
+            //transform.localScale = new Vector3(scaleX / 2, scaleY / 2, 0);
+            playerModify = 0.01F;
+        }
     }
     #endregion
 
@@ -98,8 +155,8 @@ public class ShadowMask : MonoBehaviour
         {
             if (isPlayer)
             {
-                float v = (PlayerMovement.moveDirction.x > 0) ? 0.5F : (PlayerMovement.moveDirction.x == 0) ? 0F : -0.5F;
-                float h = (PlayerMovement.moveDirction.y > 0) ? 0.5F : (PlayerMovement.moveDirction.y == 0) ? 0F : -0.5F;
+                float v = (PlayerMovement.moveDirction.x > 0) ? playerModify : (PlayerMovement.moveDirction.x == 0) ? 0F : -playerModify;
+                float h = (PlayerMovement.moveDirction.y > 0) ? playerModify : (PlayerMovement.moveDirction.y == 0) ? 0F : -playerModify;
                 //取得目標物件位置
                 float targetPositionX = targetObjectPositon.position.x + v;
                 float targetPositionY = targetObjectPositon.position.y + h;
@@ -120,13 +177,13 @@ public class ShadowMask : MonoBehaviour
 
                 for (int i = 0; i < forCount; i++)
                 {
-                    Vector3 targetPosition = new Vector3(targetPositionX + modifyX, targetPositionY + modifyY, -1);
+                    Vector3 targetPosition = new Vector3(targetPositionX + ModifyX, targetPositionY + ModifyY, -1);
                     transform.GetChild(i).position = Vector3.MoveTowards(transform.GetChild(i).position, targetPosition, darkLayoutMoveSpeed * Time.deltaTime / (i + 1));
                 }
             }
             else
             {
-                Vector3 targetPosition = new Vector3(targetObjectPositon.position.x + modifyX, targetObjectPositon.position.y + modifyY, -1);
+                Vector3 targetPosition = new Vector3(targetObjectPositon.position.x + ModifyX, targetObjectPositon.position.y + ModifyY, -1);
                 transform.position = Vector3.MoveTowards(transform.position, targetPosition, darkLayoutMoveSpeed * Time.deltaTime);
             }
         }
