423973ec62a92d5d4a442ef0833d79824f3fb2b8
MickeyHuang233 <mickeyprojectmail233@gmail.com>
2019-06-28 00:34:33 +0800

    新增NPC實時調整Z軸功能

---
+++ Public/ModifyZAxis.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModifyZAxis : MonoBehaviour
+{
+    void Start()
+    {
+        //根據自己Y軸計算Z軸的值
+        transform.position = modifyZ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //根據自己Y軸計算Z軸的值
+        transform.position = modifyZ();
+    }
+
+    private Vector3 modifyZ()
+    {
+        return new Vector3(transform.position.x, transform.position.y, transform.position.y * PlayerMovement.multiplyZ);
+    }
+}
