99e2a78389ad167217c95cc0d66f1c4f363cc514
MickeyHuang233 <mickeyprojectmail233@gmail.com>
2019-06-30 22:35:33 +0800

    新增外邊框相關素材及程式

---
+++ SpriteOutline/SpriteOutline.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteOutline : MonoBehaviour
+{
+    public Color color = Color.white;
+
+    [Range(0, 16)]
+    public int outlineSize = 1;
+
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        UpdateOutline(false);
+    }
+
+    void Update()
+    {
+        //UpdateOutline(true);
+    }
+
+    void OnEnable()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateOutline(true);
+    }
+
+    private void OnDisable()
+    {
+        UpdateOutline(false);
+    }
+
+    void UpdateOutline(bool outline)
+    {
+        MaterialPropertyBlock mpb = new MaterialPropertyBlock();
+        spriteRenderer.GetPropertyBlock(mpb);
+        mpb.SetFloat("_Outline", outline ? 1f : 0);
+        mpb.SetColor("_OutlineColor", color);
+        mpb.SetFloat("_OutlineSize", outlineSize);
+        spriteRenderer.SetPropertyBlock(mpb);
+    }
+}
