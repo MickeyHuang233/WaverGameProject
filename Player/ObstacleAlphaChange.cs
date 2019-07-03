e2337676385d4af12313a47fd94c0fa1e7db33a6
MickeyHuang233 <mickeyprojectmail233@gmail.com>
2019-07-03 23:49:25 +0800

    新增當玩家在物體後面時物體會半透明的效果

---
+++ Player/ObstacleAlphaChange.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleAlphaChange : MonoBehaviour
+{
+    #region 當物件進入觸發器時
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //將符合條件的物件加入至Map中
+        if (collision.tag.Equals("ObstacleAlphaChange")) collision.transform.parent.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 0.8F);
+    }
+    #endregion
+
+    #region 當物件離開觸發器時
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        //將符合條件的物件加入至Map中
+        if (collision.tag.Equals("ObstacleAlphaChange")) collision.transform.parent.GetComponent<Renderer>().material.color = new Color(1, 1, 1, 1);
+    }
+    #endregion
+}
