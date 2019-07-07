f653b48b7f2ae896b4b623149e5a0decc4efdb42
MickeyHuang233 <mickeyprojectmail233@gmail.com>
2019-07-07 11:36:20 +0800

    新增場景的浮動文字

---
+++ Scene/RandomShowText.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//
+public class RandomShowText : MonoBehaviour
+{
+    //要顯示的文字信息
+    public string[] showTexts;
+
+    [Header("建立文字串的數量")]
+    [Range(0F, 30F)]
+    public int createTextsNum = 5;
+
+    [Header("使用的文字預載體")]
+    public GameObject showTextObject;
+
+    [Header("文字出現范圍_XMax")]
+    [Range(-5F, 5F)]
+    public float radomXMax;
+
+    [Header("文字出現范圍_XMin")]
+    [Range(-5F, 5F)]
+    public float radomXMin;
+
+    [Header("文字出現范圍_YMax")]
+    [Range(-5F, 5F)]
+    public float radomYMax;
+
+    [Header("文字出現范圍_Min")]
+    [Range(-5F, 5F)]
+    public float radomYMin;
+
+    [Header("文字顯示顏色")]
+    public Color textColor = new Color(0F, 25F, 27F, 100F);
+
+    [Header("文字描邊顯示顏色")]
+    public Color textOutlineColor = Color.white;
+
+    //radomText的父物件
+    private Transform showTextsObject;
+
+    void Start()
+    {
+        //根據玩家Y軸計算Z軸的值
+        transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        //莪得radomText的父物件
+        showTextsObject = transform.GetChild(0).gameObject.transform;
+        //建立文字信息
+        for (int i = 1;i<= createTextsNum;i++) createRandomText(showTextsObject);
+    }
+
+    void Update()
+    {
+        if (showTextsObject.childCount < createTextsNum) createRandomText(showTextsObject);
+    }
+
+    private void createRandomText(Transform showTextsObject)
+    {
+        //文字信息顯示
+        string showText = showTexts[Random.Range(0, showTexts.Length)];
+        showTextObject.GetComponent<Text>().text = showText;
+        //文字顏色
+        showTextObject.GetComponent<Text>().color = textColor;
+
+        //生成的位置信息
+        float radomX = Random.Range(radomXMin, radomXMax) + transform.position.x;
+        float radomY = Random.Range(radomYMin, radomYMax) + transform.position.y;
+        GameObject createTextObject = Instantiate(showTextObject, new Vector3(radomX, radomY, 0), Quaternion.identity) as GameObject;
+        createTextObject.transform.SetParent(showTextsObject);//將新建出來的radomText放到showTextsObject物件底下
+    }
+}
---
+++ Scene/RandomShowTextChild.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomShowTextChild : MonoBehaviour
+{
+    //動畫信息
+    private Animator animator;
+
+    //隨機最小秒數
+    private float radomMinTime = 3F;
+
+    //隨機最大秒數
+    private float radomMaxTime = 15F;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        StartCoroutine(showTextChild());//協程_顯示文字
+    }
+
+    void Update()
+    {
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.ShowText") || animator.GetCurrentAnimatorStateInfo(0).IsName("Base Layer.HidingText"))
+        {
+            StartCoroutine(hideTextChild());//協程_隱藏並銷毀文字
+        }
+
+    }
+
+    #region 協程_顯示文字
+    IEnumerator showTextChild()
+    {
+        float radomShowWaitTime = Random.Range(radomMinTime, radomMaxTime);
+        yield return new WaitForSeconds(radomShowWaitTime);
+        setPageStatus(true);
+    }
+    #endregion
+
+    #region 協程_隱藏並銷毀文字
+    IEnumerator hideTextChild()
+    {
+        float radomHideWaitTime = Random.Range(radomMinTime, radomMaxTime * 2);
+        yield return new WaitForSeconds(radomHideWaitTime);
+        setPageStatus(false);
+        yield return new WaitForSeconds(5F);
+        Destroy(gameObject);
+    }
+    #endregion
+
+    #region 設置文字動畫狀態
+    private void setPageStatus(bool showText)
+    {
+        animator.SetBool("showText", showText);
+        animator.SetBool("hideText", !showText);
+    }
+    #endregion
+
+    #region 判斷當前菜單的狀態
+    private bool isStatus(string status, AnimatorStateInfo nowStateHash)
+    {
+        bool b = false;
+        switch (status)
+        {
+            case "CloseMenu":
+                b = nowStateHash.IsName("Base Layer.CloseMenu");
+                break;
+            case "OpenFirstMenu":
+                b = nowStateHash.IsName("Base Layer.ShowText") || nowStateHash.IsName("Base Layer.HidingText") || nowStateHash.IsName("Base Layer.Menu_03_L");
+                break;
+            case "OpenDetailMenu":
+                b = nowStateHash.IsName("Base Layer.OpenDetailMenu") || nowStateHash.IsName("Base Layer.Menu_04_L") || nowStateHash.IsName("Base Layer.Menu_06_L");
+                break;
+            default:
+                Debug.Log(status + "找不到相對應的狀態，可能輸入錯誤");
+                break;
+        }
+        return b;
+    }
+    #endregion
+}
