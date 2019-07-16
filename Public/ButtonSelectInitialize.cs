34903d203fbedd3581b62b9310e2df0a4b4bcfff
MickeyHuang233 <mickeyprojectmail233@gmail.com>
2019-07-16 23:24:34 +0800

    新增UGUI的鍵盤控制

---
+++ Public/ButtonSelectInitialize.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+/*
+ UGUI的鍵盤控制
+*/
+public class ButtonSelectInitialize : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onStart;
+
+    void Start()
+    {
+        this.onStart.Invoke();
+    }
+}
